b909cc16f82272ed96060bca3140d814866ee950
Hakan Ünlü <unluhak@outlook.com>
2021-10-03 22:41:19 +0200

    Fertig

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">Click ME</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click ME</div>
 </template>
 
 <script>
@@ -8,12 +8,26 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer();
     }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit('end', this.reactionTime);
+    },
   },
 };
 </script>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Reaction time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = '🤯';
+    } else if (this.score < 400) {
+      this.rank = '😕';
+    } else {
+      this.rank = '❗';
+    }
+  },
+};
+</script>
+
+<style scoped>
+.rank {
+  font-size: 1.4em;
+  color: chocolate;
+  font-weight: bold;
+}
+</style>
